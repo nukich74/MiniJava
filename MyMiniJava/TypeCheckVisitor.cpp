cbcfd837a7e02cb1b4814414dbfb21975365c307
Astashkin Arseny <ars.astashkin@gmail.com>
2014-12-15 18:49:06 +0300

    Добавлен шаблон Visitor-а для проверки типов.

---
+++ MyMiniJava/TypeCheckVisitor.cpp
@@ -0,0 +1,3 @@
+#include "TypeCheckVisitor.h"
+
+
---
+++ MyMiniJava/TypeCheckVisitor.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "iVisitor.h"
+#include "syntaxTree.h"
+
+namespace SymbolsTable {
+
+	class CTCVisitor : public IVisitor {
+	public:
+		void Visit( const CProgram& p );
+		void Visit( const CMainClass& p );
+
+		void Visit( const CClassDecl& p );
+		void Visit( const CExtendClassDecl& p );
+		void Visit( const CVarDecl& p );
+		void Visit( const CMethodDecl& p );
+
+		void Visit( const CGroupStmt& p );
+		void Visit( const CIfStmt& p );
+		void Visit( const CWhileStmt& p );
+		void Visit( const CSOPStmt& p );
+		void Visit( const CAssignStmt& p );
+		void Visit( const CAssignExprStmt& p );
+
+		void Visit( const COpExpr& p );
+		void Visit( const CExExpr& p );
+		void Visit( const CMethodCallExpr& p );
+		void Visit( const CIntExpr& p );
+		void Visit( const CTrueExpr& p );
+		void Visit( const CFalseExpr& p );
+		void Visit( const CNameExpr& p );
+		void Visit( const CThisExpr& p );
+		void Visit( const CNewIntExpr& p );
+		void Visit( const CNewIdExpr& p );
+		void Visit( const CNotExpr& p );
+		void Visit( const CBrExpr& p );
+		void Visit( const CIdExpr& p );
+		void Visit( const CLengthExpr& p );
+		void Visit( const CUnaryMinusExpr& p );
+
+
+		void Visit( const CTypeName& p );
+
+		void Visit( const CExprList& p );
+		void Visit( const CFormalList& p );
+		void Visit( const CClassDeclList& p );
+		void Visit( const CVarDeclList& p );
+		void Visit( const CMethodDeclList& p );
+		void Visit( const CStmtList& p );
+
+	};
+
+}	
+
+
+
+
