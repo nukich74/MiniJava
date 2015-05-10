9fc322258d07d6edd54a00e150f32e7f2a118497
Valeriy <stelz40494@gmail.com>
2015-05-10 17:10:20 +0400

    Added dummy workflow graph
    My 1st commit ^^

---
+++ MyMiniJava/LivenessAnalysis.cpp
@@ -0,0 +1,83 @@
+﻿// Автор: Федюнин Валерий
+// Набор классов для анализа времени жизни переменных
+
+#include "LivenessAnalysis.h"
+
+#include <assert.h>
+
+
+namespace Assembler {
+
+// =====================================================================================================================
+
+CGraph::CGraph( int size ) : nodes( size ) {}
+
+
+void CGraph::AddEdge( int from, int to )
+{
+	assert( from < int( nodes.size() ) );
+	assert( to < int( nodes.size() ) );
+
+	nodes[from].out.push_back( to );
+	nodes[to].in.push_back( from );
+}
+
+
+int CGraph::Size() const
+{
+	return nodes.size();
+}
+
+
+const CNode& CGraph::GetNode( int index ) const
+{
+	return nodes[index];
+}
+
+// =====================================================================================================================
+
+CWorkFlowGraph::CWorkFlowGraph( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions ) : graph( 0 )
+{
+	buildNodes( asmFunctions );
+	buildMaps( asmFunctions );
+}
+
+
+// строит граф из необходимого количества без ребер
+void CWorkFlowGraph::buildNodes( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions )
+{
+	int graphSize = 0;
+	for( auto func : asmFunctions ) {
+		graphSize += func.second.size();
+	}
+	graph = CGraph( graphSize );
+}
+
+
+// строит соответствия, необходимые для построения графа
+void CWorkFlowGraph::buildMaps( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions )
+{
+	int nodeIndex = 0;
+	for( auto func : asmFunctions ) {
+		functions.insert( std::make_pair( func.first, nodeIndex ) );
+		for( auto cmd : func.second ) {
+			// TODO: добавить какую-либо нормальную проверку того, что инструкция является меткой
+			std::string command = cmd->FormatInstr( Temp::CTempMap() );
+			if( command.length() > 1 && command.back() == ':' ) {
+				// если инструкция является меткой то добавим ее в соответствие
+				labels.insert( std::make_pair( command.substr( 0, command.length() - 1 ), nodeIndex ) );
+			}
+			++nodeIndex;
+		}
+	}
+}
+
+
+// строит ребра в графе
+void CWorkFlowGraph::addEdges()
+{
+	// TODO:
+	// для начала необходимо разобраться, как можно выведать из инструкции ее тип
+}
+
+}
---
+++ MyMiniJava/LivenessAnalysis.h
@@ -0,0 +1,68 @@
+﻿// Автор: Федюнин Валерий
+// Набор классов для анализа времени жизни переменных
+
+#pragma once
+
+#include "AssemblerInstr.h"
+
+#include <set>
+#include <string>
+#include <vector>
+#include <map>
+
+
+namespace Assembler {
+// Вершина графа
+struct CNode {
+	// входящие ребра
+	std::vector<int> in;
+	// исходящие ребра
+	std::vector<int> out;
+};
+
+
+// Граф
+class CGraph {
+public:
+	explicit CGraph( int size );
+	
+	// добавить ребро в граф из вершины from в вершину to
+	void AddEdge( int from, int to );
+
+	// Количество вершин в графе
+	int Size() const;
+	
+	// получить вершину
+	const CNode& GetNode( int index ) const;
+
+private:
+	// узлы графа
+	// вся информация о ребрах хранится внутри вершин
+	std::vector<CNode> nodes;
+};
+
+
+// Граф потока управления
+class CWorkFlowGraph {
+public:
+	// Конструктор
+	// принимает на вход map где ключ - имя функции а значение - список ассемблерных команд
+	explicit CWorkFlowGraph( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions );
+
+private:
+	// граф
+	CGraph graph;
+
+	// соответствие между метками и вершинами графа
+	std::map<std::string, int> labels;
+
+	// соответствие между функциями и вершинами графа
+	std::map<std::string, int> functions;
+
+	void buildNodes( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions );
+	void buildMaps( const std::map<std::string, std::list<const IAsmInstr*>>& asmFunctions );
+	void addEdges();
+};
+
+
+} // namespace Assembler
